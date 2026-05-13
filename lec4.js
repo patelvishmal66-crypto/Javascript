b8203ea4886886f8f8bc86f2d9bfd4ccb50a5cd6
Vishmal patel <patelvishmal66@gmail.com>
2026-05-13 14:08:21 +0530

    lec6.html

--- lec4.js
+++ lec4.js
@@ -1,20 +1,20 @@
 // DOM
 console.log("kyaa");
-// let btn = document.querySelector('button');
-// btn.style.backgroundColor = 'rgb(8, 8, 8)';
-// btn.style.color = 'white';
-// let body = document.querySelector('body');
-
-// btn.addEventListener('click', function(){
-//     // console.log(this);
-
-//     body.style.backgroundColor = 'rgb(8, 8, 8)';
-// })  
-
-let h1= document.getElementsByClassName("two")
-
 let btn = document.querySelector('button');
+btn.style.backgroundColor = 'rgb(8, 8, 8)';
+btn.style.color = 'white';
+let body = document.querySelector('body');
 
-btn.addEventListener("click",()=>{ 
+btn.addEventListener('click', function(){
+    // console.log(this);
+
+    body.style.backgroundColor = 'rgb(8, 8, 8)';
+})  
+
+// let h1= document.getElementsByClassName("two")
+
+// let btn = document.querySelector('button');
+
+// btn.addEventListener("click",()=>{ 
      
- })
+//  })
